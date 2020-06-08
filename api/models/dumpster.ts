82be95a0d29c1c052a0b13b24fa06bf7fe96d488
plus-plus-hyp3r-n0vA <52785393+plus-plus-hyp3r-n0vA@users.noreply.github.com>
2020-06-08 13:12:57 +0300

    Modified dumpster garbageTypes

--- api/models/dumpster.ts
+++ api/models/dumpster.ts
@@ -2,17 +2,17 @@ import DatabaseConnection from '../core/database.ts'
 
 export default class Dumpster {
     #id: number | null
-    #garbageType: string | null
+    #garbageTypes: Array<string> | null
     address: string
     lon: number
     lat: number
-    active: number
+    active: boolean
     dateCreated: Date
     dateUpdated: Date | null
 
-    constructor(address: string, lon: number, lat: number, active: number, dateCreated: Date, dateUpdated: Date|null = null) {
+    constructor(address: string, lon: number, lat: number, active: boolean, dateCreated: Date, dateUpdated: Date|null = null) {
         this.#id = null
-        this.#garbageType = null
+        this.#garbageTypes = null
         this.address = address
         this.lon = lon
         this.lat = lat
@@ -25,18 +25,22 @@ export default class Dumpster {
         return this.#id;
     }
 
-    get garbageType() {
-        if(this.#garbageType !== null)
-            return this.#garbageType
+    get garbageTypes() {
+        if(this.#garbageTypes !== null)
+            return this.#garbageTypes
         return (async () => {
             let client = new DatabaseConnection()
-            const query = (await client.query("select type from public_dumpster d join garbage_type g \
-                                               on d.garbage_type_id = g.id where d.id = ? limit 1", [this.#id]))[0]
+            this.#garbageTypes = []
+
+            const query = await client.query("select type from public_dumpster d join garbage_type g \
+                                               on d.garbage_type_id = g.id where d.id = ?", [this.#id])
             client.close()
-            if (!query) return null
-            
-            this.#garbageType = query.type
-            return query.type
+            if (!query[0]) return null
+
+            for (const row of query)
+                this.#garbageTypes.push(row.type)
+                
+            return this.#garbageTypes
         })()
     }
 
@@ -46,16 +50,16 @@ export default class Dumpster {
         client.close()
         if (!query) return null
 
-        let dump_addr = new Dumpster(query.address, query.lon, query.lat, query.active,
+        let dumpAddr = new Dumpster(query.address, query.lon, query.lat, query.active,
                             new Date(query.date_created), query.date_updated ? new Date(query.date_updated) : null)
-        dump_addr.#id = query.id
+        dumpAddr.#id = query.id
 
-        return dump_addr
+        return dumpAddr
     }
 
-    static async getAllDumpsters() {
+    static async getAllActiveDumpsters() {
         let client = new DatabaseConnection()
-        const query = await client.query("select * from dumpster_address")
+        const query = await client.query("select * from dumpster_address where active = 1")
         client.close()
         if (!query[0]) return null
         let dumpsterArr = []
